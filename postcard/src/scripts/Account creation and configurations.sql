7dbe09184db9852d6851939be3747f6debbbf5d1
narasimhacalsoft <narasimha.bdvl@gmail.com>
2020-07-09 14:04:00 +0530

    Update Account creation and configurations.sql
    Changed Script file

--- postcard/src/scripts/Account creation and configurations.sql
+++ postcard/src/scripts/Account creation and configurations.sql
@@ -20,7 +20,7 @@ CREATE TABLE `postcard`.`postcard` (
   `cardStatus` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`cardId`),
 CONSTRAINT order_idfk_1 FOREIGN KEY (`orderId`) REFERENCES `postcard`.`postcardorder` (`orderId`));
-);
+
 
 CREATE TABLE `postcard`.`postcardorder` (
   `orderId` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,13 +31,13 @@ CREATE TABLE `postcard`.`postcardorder` (
   `brandingJson` text DEFAULT NULL,
   PRIMARY KEY (`orderId`),
 CONSTRAINT image_idfk_1 FOREIGN KEY (`imageId`) REFERENCES `postcard`.`image` (`imageId`));	
-);
+
 
 CREATE TABLE `postcard`.`image` (
   `imageId` int(11) NOT NULL AUTO_INCREMENT,
   `image` longblob DEFAULT NULL,
   `imageType` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`image_id`)
+  PRIMARY KEY (`imageId`)
 ); 
 
 CREATE TABLE `postcard`.`user` (
@@ -55,43 +55,43 @@ CREATE TABLE `postcard`.`VALIDATION_CONFIGURATION` (
     primary key (`id`)
 );
 
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (1,'enable-swagger','Y');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (2,'authURL','https://apiint.post.ch/OAuth/authorization');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (3,'accessTokenURL','https://apiint.post.ch/OAuth/token');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (4,'clientID','3ff6558b0da85758e9d36fdd2092cc19');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (5,'clientSecret','3552aa3d6dcb17e93353943758b6d63f');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (6,'scope','PCCAPI');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (7,'postcardAPI','api/v1/postcards/');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (8,'campaignKey','0b6eee81-5ea3-475a-975b-0318335eb228');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (9,'createPostcardEndPoint','api/v1/postcards?campaignKey=');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (10,'campaignsEndPoint','api/v1/campaigns/');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (11,'statisticEndpoint','/statistic');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (12,'postcardBaseURL','https://apiint.post.ch/pcc/');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (13,'stateEndPoint','/state');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (14,'approvalEndPoint','/approval');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (15,'senderAddressEndPoint','/addresses/sender');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (16,'recipientAddressEndPoint','/addresses/recipient');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (17,'frontImageEndPoint','/image');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (18,'senderTextEndPoint','/sendertext?senderText=');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (19,'brandingTextEndPoint','/branding/text');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (20,'frontPreviewsEndPoint','/previews/front');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (21,'backPreviewsEndPoint','/previews/back');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (22,'brandingImageEndPoint','/branding/image');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (23,'stampImageEndPoint','/stamp/image');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (24,'getUserQuery','select * from user where userName=?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (25,'jwt.secret','AltenCalsoftLabs');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (26,'updateProperty','update PROPERTIES set value=? where name =?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (27,'getValidationConfiguration','select `configuration` from VALIDATION_CONFIGURATION where `key`=?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (28,'createPostcardOrderQuery','insert into postcardorder(senderText,senderJson,brandingText,brandingJson) values (?,?,?,?)');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (29,'updatePostcardOrderQuery','update postcardorder set senderText =? ,senderJson=? ,brandingText=?,brandingJson=? where orderId = ?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (30,'findOnePostcardOrderQuery','SELECT * FROM postcardorder WHERE orderId = ?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (31,'findallPostcardOrderQuery','select * from postcardorder');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (32,'deletePostcardOrderQuery','delete from postcardorder where orderId = ?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (33,'createPostcardQuery','insert into postcard(orderId,cardKey,recipientJson,submissionStatus,response,attempts,cardStatus) values (?,?,?,?,?,?,?)');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (34,'updatePostcardQuery','update postcard set recipientJson =? ,submissionStatus=? ,response=?,attempts=?,cardStatus=? where cardKey = ?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (35,'findallPostcardQuery','select * from postcard');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (36,'deletePostcardQuery','delete from Postcard where cardId = ?');
-INSERT INTO `postcard`.`PROPERTIES` (`id`,`name`,`value`) VALUES (37,'findOnePostcardQuery','SELECT * FROM Postcard WHERE cardId = ?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('enable-swagger','Y');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('authURL','https://apiint.post.ch/OAuth/authorization');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('accessTokenURL','https://apiint.post.ch/OAuth/token');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('clientID','3ff6558b0da85758e9d36fdd2092cc19');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('clientSecret','3552aa3d6dcb17e93353943758b6d63f');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('scope','PCCAPI');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('postcardAPI','api/v1/postcards/');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('campaignKey','0b6eee81-5ea3-475a-975b-0318335eb228');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('createPostcardEndPoint','api/v1/postcards?campaignKey=');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('campaignsEndPoint','api/v1/campaigns/');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('statisticEndpoint','/statistic');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('postcardBaseURL','https://apiint.post.ch/pcc/');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('stateEndPoint','/state');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('approvalEndPoint','/approval');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('senderAddressEndPoint','/addresses/sender');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('recipientAddressEndPoint','/addresses/recipient');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('frontImageEndPoint','/image');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('senderTextEndPoint','/sendertext?senderText=');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('brandingTextEndPoint','/branding/text');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('frontPreviewsEndPoint','/previews/front');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('backPreviewsEndPoint','/previews/back');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('brandingImageEndPoint','/branding/image');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('stampImageEndPoint','/stamp/image');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('getUserQuery','select * from user where userName=?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('jwt.secret','AltenCalsoftLabs');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('updateProperty','update PROPERTIES set value=? where name =?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('getValidationConfiguration','select `configuration` from VALIDATION_CONFIGURATION where `key`=?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('createPostcardOrderQuery','insert into postcardorder(senderText,senderJson,brandingText,brandingJson) values (?,?,?,?)');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('updatePostcardOrderQuery','update postcardorder set senderText =? ,senderJson=? ,brandingText=?,brandingJson=? where orderId = ?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('findOnePostcardOrderQuery','SELECT * FROM postcardorder WHERE orderId = ?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('findallPostcardOrderQuery','select * from postcardorder');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('deletePostcardOrderQuery','delete from postcardorder where orderId = ?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('createPostcardQuery','insert into postcard(orderId,cardKey,recipientJson,submissionStatus,response,attempts,cardStatus) values (?,?,?,?,?,?,?)');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('updatePostcardQuery','update postcard set recipientJson =? ,submissionStatus=? ,response=?,attempts=?,cardStatus=? where cardKey = ?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('findallPostcardQuery','select * from postcard');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('deletePostcardQuery','delete from Postcard where cardId = ?');
+INSERT INTO `postcard`.`PROPERTIES` (`name`,`value`) VALUES ('findOnePostcardQuery','SELECT * FROM Postcard WHERE cardId = ?');
 
 insert into `postcard`.`user`(`userName`,`password`,`role`)values('alten','$2a$10$ixlPY3AAd4ty1l6E2IsQ9OFZi2ba9ZQE0bP7RFcGIWNhyFrrT3YUi','ROLE_USER');
 insert into `postcard`.`user`(`userName`,`password`,`role`)values('admin','$2a$10$ixlPY3AAd4ty1l6E2IsQ9OFZi2ba9ZQE0bP7RFcGIWNhyFrrT3YUi','ROLE_ADMIN');
