6c39a2a1b57de6e061ae59243d9fe01ef25f55bd
narasimhulu.k <narasimhulu.k@altencalsoftlabs.com>
2020-07-14 19:16:45 +0530

    added for getallPostcards

---
+++ postcard/src/main/java/com/postcard/model/PostcardOrderMapper.java
@@ -0,0 +1,26 @@
+package com.postcard.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class PostcardOrderMapper implements RowMapper<PostcardOrder> {
+
+	@Override
+	public PostcardOrder mapRow(ResultSet rs, int rowNum) throws SQLException {
+		PostcardOrder postcardOrder = new PostcardOrder();
+		postcardOrder.setOrderId(rs.getInt("orderId"));
+		postcardOrder.setSenderText(rs.getString("senderText"));
+		postcardOrder.setSenderJson(rs.getString("senderJson"));
+		postcardOrder.setBrandingText(rs.getString("brandingText"));
+		postcardOrder.setBrandingJson(rs.getString("brandingJson"));
+		postcardOrder.setCreatedDate(rs.getTimestamp("createdDate"));
+		postcardOrder.setCreatedBy(rs.getString("createdBy"));
+		postcardOrder.setUpdatedDate(rs.getTimestamp("updatedDate"));
+		postcardOrder.setUpdatedBy(rs.getString("updatedBy"));
+		postcardOrder.setUpdatedBy(rs.getString("orderStatus"));
+		return postcardOrder;
+	}
+
+}
